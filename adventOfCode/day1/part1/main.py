861d275a13186d93e33fd6e9643c8b478e762292
Andrea Parker <home@Andreas-MacBook-Pro.local>
2023-12-06 18:39:27 -0500

    working code

--- adventOfCode/day1/part1/main.py
+++ adventOfCode/day1/part1/main.py
@@ -1,34 +1,18 @@
-def findAll(aStr, sub) -> None:
-    start = 0
-    while True:
-        start = aStr.find(sub, start)
-        if start == -1: 
-            return
-        yield start
-        start += len(sub)
+digitMap = { 'oneight' : '18', 'twone' : '21', 'threeight' : '38', 'fiveight': '58',
+            'sevenine' : '79', 'eightwo' : '82', 'eighthree' : '83', 'nineight' : '98',
+            'one' : '1', 'two': '2', 'three' : '3', 'four' : '4', 'five' : '5', 
+            'six' : '6', 'seven' : '7', 'eight' : '8', 'nine' : '9'}
+
+def extract(line: str) -> int:
+    for digit in digitMap:
+        line = line.replace(digit, digitMap[digit])
+    digits = [s for s in line if s.isnumeric()]
+    return int(digits[0] + digits[-1])
 
 def main() -> None:
-    numWords = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-    with open('/Users/home/Desktop/python/adventOfCode/day1/day1part1/nums.txt') as f:
-        line = f.read().split()
-        final = []
-        for e in line:
-            indexes = {}
-            for numWord in numWords:
-                matches = list(findAll(e, numWord))
-                for m in matches:
-                    indexes[m] = nums[numWords.index(numWord)]
-            for num in nums:
-                matches = list(findAll(e, str(num)))
-                for m in matches:
-                    indexes[m] = num
-            first = str(indexes[min(indexes)])
-            first += str(indexes[max(indexes)])
-            final.append(int(first))
-    f.close()
-    print(sum(final))
+    file = open('nums.txt', 'r')
+    digits = [extract(l) for l in file.split()]
+    print(sum(digits))
 
 if __name__ == "__main__":
     main()
