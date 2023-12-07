99cdbf61d5d7d97293a96c06dd0e1fa36e30c641
Andrea Parker <home@Andreas-MacBook-Pro.local>
2023-12-06 19:20:20 -0500

    debug prints

--- adventOfCode/day1/part1/main.py
+++ adventOfCode/day1/part1/main.py
@@ -1,14 +1,18 @@
-digitMap = { 'oneight' : '18', 'twone' : '21', 'threeight' : '38', 'fiveight': '58',
-            'sevenine' : '79', 'eightwo' : '82', 'eighthree' : '83', 'nineight' : '98',
+digitMap = { 'oneight' : '18', 'oneightwo' : '182', 'twone' : '21', 'threeight' : '38', 'fiveight': '58',
+            'sevenine' : '79', 'eightwo' : '82', 'eightwone' : '821', 'eighthree' : '83', 'nineight' : '98', 'nineightwo' : '982',
             'one' : '1', 'two': '2', 'three' : '3', 'four' : '4', 'five' : '5', 
             'six' : '6', 'seven' : '7', 'eight' : '8', 'nine' : '9'}
 
 def extract(line: str) -> int:
     for digit in digitMap:
         line = line.replace(digit, digitMap[digit])
-    
-    digits = [s for s in line if s.isnumeric()]
-    return int(digits[0] + digits[-1])
+    with open('numRes.txt', 'a') as file:
+        digits = [s for s in line if s.isnumeric()]
+        x = int(str(digits[0]) + str(digits[-1]))
+        print(digits, file=file)
+        print(x, file=file)
+    file.close()
+    return x
 
 def main() -> None:
     file = open('/Users/home/Desktop/python/adventOfCode/adventOfCode/day1/part1/nums.txt')
